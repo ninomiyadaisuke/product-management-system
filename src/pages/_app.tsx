d38c370b9e4c3d6ddbde985484fc5f978672f1ec
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-10-06 10:34:33 +0900

    scss initialization

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'src/styles/base.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
