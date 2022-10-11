81a860293d9e866cdc95ecc1cf6dca7165823fdb
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2022-10-11 13:39:06 +0900

    create header component

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -28,197 +28,25 @@ const Header: FC = () => {
         <ToggleButton onClick={handleClick} toggle={toggle} active={active} />
       </div>
 
-      {tablet && (
-        <div className={styles.header__hamburger}>
-          <HamburgerButton />
-        </div>
-      )}
+      <div className={styles.header__hamburger}>
+        <HamburgerButton />
+      </div>
+
       {/* <ToggleButton /> */}
       {/* <!-- Header Menu --> */}
-      <ul className="nav user-menu">
+      <ul>
         {/* <!-- Search --> */}
-        {/* <li className="nav-item">
+        <li className={styles.header__item}>
           <NavSearch />
-        </li> */}
+        </li>
         {/* <!-- /Search --> */}
-
-        {/* <!-- Notifications --> */}
-        {/* <li className="nav-item dropdown">
-          <a href="javascript:void(0);" className="dropdown-toggle nav-link" data-bs-toggle="dropdown">
-            <Image src="/icons/notification-bing.svg" alt="img" height={40} width={40} />{' '}
-            <span className="badge rounded-pill">4</span>
-          </a>
-          <div className="dropdown-menu notifications">
-            <div className="topnav-dropdown-header">
-              <span className="notification-title">Notifications</span>
-              <a href="javascript:void(0)" className="clear-noti">
-                {' '}
-                Clear All{' '}
-              </a>
-            </div>
-            <div className="noti-content">
-              <ul className="notification-list">
-                <li className="notification-message">
-                  <a href="activities.html">
-                    <div className="media d-flex">
-                      <span className="avatar flex-shrink-0">
-                        <Image alt="" src="/profiles/avatar-02.jpg" height={40} width={40} />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">John Doe</span> added new task{' '}
-                          <span className="noti-title">Patient appointment booking</span>
-                        </p>
-                        <p className="noti-time">
-                          <span className="notification-time">4 mins ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li className="notification-message">
-                  <a href="activities.html">
-                    <div className="media d-flex">
-                      <span className="avatar flex-shrink-0">
-                        <Image alt="" src="/profiles/avatar-03.jpg" height={40} width={40} />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Tarah Shropshire</span> changed the task name
-                          <span className="noti-title">Appointment booking with payment gateway</span>
-                        </p>
-                        <p className="noti-time">
-                          <span className="notification-time">6 mins ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li className="notification-message">
-                  <a href="activities.html">
-                    <div className="media d-flex">
-                      <span className="avatar flex-shrink-0">
-                        <Image alt="" src="/profiles/avatar-06.jpg" height={40} width={40} />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Misty Tison</span> added{' '}
-                          <span className="noti-title">Domenic Houston</span> and{' '}
-                          <span className="noti-title">Claire Mapes</span>
-                          to project <span className="noti-title">Doctor available module</span>
-                        </p>
-                        <p className="noti-time">
-                          <span className="notification-time">8 mins ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li className="notification-message">
-                  <a href="activities.html">
-                    <div className="media d-flex">
-                      <span className="avatar flex-shrink-0">
-                        <Image alt="" src="/profiles/avatar-17.jpg" height={40} width={40} />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Rolland Webber</span> completed task{' '}
-                          <span className="noti-title">Patient and Doctor video conferencing</span>
-                        </p>
-                        <p className="noti-time">
-                          <span className="notification-time">12 mins ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-                <li className="notification-message">
-                  <a href="activities.html">
-                    <div className="media d-flex">
-                      <span className="avatar flex-shrink-0">
-                        <Image alt="" src="/profiles/avatar-13.jpg" height={40} width={40} />
-                      </span>
-                      <div className="media-body flex-grow-1">
-                        <p className="noti-details">
-                          <span className="noti-title">Bernardo Galaviz</span> added new task{' '}
-                          <span className="noti-title">Private chat module</span>
-                        </p>
-                        <p className="noti-time">
-                          <span className="notification-time">2 days ago</span>
-                        </p>
-                      </div>
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="topnav-dropdown-footer">
-              <a href="activities.html">View all Notifications</a>
-            </div>
-          </div>
-        </li> */}
-        {/* <!-- /Notifications --> */}
-
-        {/* <li className="nav-item dropdown has-arrow main-drop">
-          <a href="javascript:void(0);" className="dropdown-toggle nav-link userset" data-bs-toggle="dropdown">
-            <span className="user-img">
-              <Image src="/profiles/avator1.jpg" alt="" height={40} width={40} />
-              <span className="status online"></span>
-            </span>
-          </a>
-          <div className="dropdown-menu menu-drop-user">
-            <div className="profilename">
-              <div className="profileset">
-                <span className="user-img">
-                  <Image src="/profiles/avator1.jpg" alt="" height={40} width={40} />
-                  <span className="status online"></span>
-                </span>
-                <div className="profilesets">
-                  <h6>John Doe</h6>
-                  <h5>Admin</h5>
-                </div>
-              </div>
-              <hr className="m-0" />
-              <a className="dropdown-item" href="profile.html">
-                {' '}
-                <i className="me-2" data-feather="user"></i> My Profile
-              </a>
-              <a className="dropdown-item" href="generalsettings.html">
-                <i className="me-2" data-feather="settings"></i>Settings
-              </a>
-              <hr className="m-0" />
-              <a className="dropdown-item logout pb-0" href="signin.html">
-                <Image src="/icons/log-out.svg" className="me-2" alt="img" height={40} width={40} />
-                Logout
-              </a>
-            </div>
-          </div>
-        </li> */}
       </ul>
       {/* <!-- /Header Menu --> */}
 
       {/* <!-- Mobile Menu --> */}
-      {/* <div className="dropdown mobile-user-menu">
-        <a
-          href="javascript:void(0);"
-          className="nav-link dropdown-toggle"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <i className="fa fa-ellipsis-v"></i>
-        </a>
-        <div className="dropdown-menu dropdown-menu-right">
-          <a className="dropdown-item" href="profile.html">
-            My Profile
-          </a>
-          <a className="dropdown-item" href="settings.html">
-            Settings
-          </a>
-          <a className="dropdown-item" href="index.html">
-            Logout
-          </a>
-        </div>
-      </div> */}
+      <a className={styles.header__ellipsis}>
+        <i className="fa fa-ellipsis-v"></i>
+      </a>
       {/* <!-- /Mobile Menu --> */}
     </header>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,22 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Script from 'next/script';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'src/styles/base.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Script src="https://kit.fontawesome.com/fbadad80a0.js" />
+    </>
+  );
 }
 
 export default MyApp;
